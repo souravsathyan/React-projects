0b673309b5c3090a26c6da2d83091019abf33eb9
souravsathyan <souravksathyan@gmail.com>
2024-02-04 11:08:30 +0530

    redux persist added

--- Auth-project/client/src/App.jsx
+++ Auth-project/client/src/App.jsx
@@ -1,16 +1,19 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
-import {store } from "./redux/store.js"
+import {store,persistor } from "./redux/store.js"
 import {Provider} from "react-redux"
+import {PersistGate} from "redux-persist/integration/react"
 
 function App() {
   return (
    <Provider store={store}>
+    <PersistGate persistor={persistor} loading={null} >
       <div className="px-[100px]">
         <Header />
         <Outlet />
       </div>
+      </PersistGate>
     </Provider>
   );
 }
