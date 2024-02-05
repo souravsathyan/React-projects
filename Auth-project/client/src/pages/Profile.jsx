c2e1967151156bad43cee8bf1b7833ffd66fb7ef
souravsathyan <souravksathyan@gmail.com>
2024-02-05 14:57:18 +0530

    update functionality complete

--- Auth-project/client/src/pages/Profile.jsx
+++ Auth-project/client/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useDispatch ,useSelector } from "react-redux";
 import { useEffect, useRef, useState } from "react";
 import { app } from "../firebase";
 import {
@@ -7,16 +7,22 @@ import {
   ref,
   uploadBytesResumable,
 } from "firebase/storage";
+import {
+  updateUserFailure,updateUserStart,updateUserSuccess
+} from "../redux/userSlice"
 
 const Profile = () => {
+
   const {
-    user: { currentUser },
+    user: { currentUser },loading,error
   } = useSelector((state) => state.user);
   const fileRef = useRef(null);
   const [image, setImage] = useState(undefined);
   const [imagePercentage, setImagePercentage] = useState(0);
   const [imageError, setImageError] = useState(false);
   const [formData, setFormData] = useState({});
+  const dispatch = useDispatch()
+  const [updateSuccess, setUpdateSuccess] = useState(false)
 
   useEffect(() => {
     if (image) {
@@ -45,11 +51,39 @@ const Profile = () => {
         })
     );
   };
+
+  const handleChange = (e)=>{
+    setFormData({...formData,[e.target.id]:e.target.value})
+    console.log(formData)
+  }
+
+  const handleSubmit =async (e)=>{
+    e.preventDefault()
+    try{
+      dispatch(updateUserStart)
+      const res = await fetch(`/api/user/update/${currentUser._id}`,{
+        method:"POST",
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify(formData)
+      })
+      const data = await res.json()
+      if(data.status===false){
+        dispatch(updateUserFailure(data))
+      }
+      dispatch(updateUserSuccess(data))
+      setUpdateSuccess(true)
+    }catch(err){
+      dispatch(updateUserFailure(err))
+    }
+  }
+
   return (
     <>
       <div className="p-3 max-w-lg mx-auto">
         <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-        <form className="flex flex-col gap-4">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
           <input
             type="file"
             hidden
@@ -93,6 +127,7 @@ const Profile = () => {
             id="username"
             placeholder="Username"
             className="bg-slate-100 rounded-lg p-3"
+            onChange={handleChange}
           />
           <input
             defaultValue={currentUser.email}
@@ -101,18 +136,26 @@ const Profile = () => {
             placeholder="Email"
             className="bg-slate-100 rounded-lg p-3"
             autoComplete="on"
+            onChange={handleChange}
           />
           <input
             type="password"
             id="password"
             placeholder="Password"
             className="bg-slate-100 rounded-lg p-3"
+            onChange={handleChange}
           />
+          <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          {loading ? 'Loading...' : 'Update'}
+        </button>
         </form>
         <div className="flex justify-between mt-5">
           <span className="text-red-700 cursor-pointer">Delete Account</span>
           <span className="text-red-700 cursor-pointer">Sign out</span>
         </div>
+        <p className="text-red-600 mt-5">{error && 'Something went wrong'}</p>
+        <p className="text-green-600 mt-5">{updateSuccess && 'User updated successfully'}</p>
+
       </div>
     </>
   );
--- Auth-project/client/src/redux/userSlice.js
+++ Auth-project/client/src/redux/userSlice.js
@@ -1,32 +1,45 @@
-import {createSlice} from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
     currentUser: null,
     loading: false,
     error: false,
-  };
+};
 
 const userSlice = createSlice(
     {
-        name:"user",
+        name: "user",
         initialState,
-        reducers:{
-            loginStart:(state)=>{
-                state.loading=true
+        reducers: {
+            loginStart: (state) => {
+                state.loading = true
             },
-            loginSuccess:(state,action)=>{
+            loginSuccess: (state, action) => {
                 state.loading = false,
-                state.error=false
+                    state.error = false
                 state.currentUser = action.payload
             },
-            loginFailure:(state,action)=>{
-                state.loading=false,
-                state.error = action.payload
+            loginFailure: (state, action) => {
+                state.loading = false,
+                    state.error = action.payload
+            },
+            updateUserStart: (state) => {
+                state.loading = false
+            },
+            updateUserSuccess: (state, action) => {
+                state.loading = false,
+                    state.error = false
+                state.currentUser = action.payload
+            },
+            updateUserFailure: (state, action) => {
+                state.loading = false,
+                    state.error = action.payload
             }
+
         }
     }
 )
 
-export const {loginStart, loginSuccess, loginFailure } = userSlice.actions
+export const { loginStart, loginSuccess, loginFailure, updateUserFailure, updateUserStart, updateUserSuccess } = userSlice.actions
 
 export default userSlice.reducer
